c209773e41c7a4f3e0bfa017c3cd090b6ed1e185
ropeiz2663 <ropeiz2663@gmail.com>
2024-11-05 13:08:27 +0100

    Corrections in piece.java file v2

--- src/main/java/model/Piece.java
+++ src/main/java/model/Piece.java
@@ -2,6 +2,9 @@ package model;
 
 import java.util.Random;
 
+/**
+ * Clase que representa una pieza del juego Tetris.
+ */
 public class Piece {
     /** Matriz booleana que representa la forma de la pieza. */
     private boolean[][] shape;
@@ -11,8 +14,7 @@ public class Piece {
 
     /** Coordenada Y de la posición actual de la pieza en el tablero. */
     private int y;
-}
-   
+
     /**
      * Constructor de la clase Piece.
      *
@@ -68,71 +70,75 @@ public class Piece {
         this.y += dy;
     }
 
+    // Rotación de la pieza en el sentido de las agujas del reloj
+    public void rotateClockwise() {
+        int rows = shape.length;
+        int cols = shape[0].length;
+        boolean[][] rotatedShape = new boolean[cols][rows];
 
-	// Rotación de la pieza en el sentido de las agujas del reloj
-	public void rotateClockwise() {
-	    int rows = shape.length;
-	    int cols = shape[0].length;
-	    boolean[][] rotatedShape = new boolean[cols][rows];
-	
-	    for (int i = 0; i < rows; i++) {
-	        for (int j = 0; j < cols; j++) {
-	            rotatedShape[j][rows - 1 - i] = shape[i][j];
-	        }
-	    }
-	    shape = rotatedShape;
-	}
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                rotatedShape[j][rows - 1 - i] = shape[i][j];
+            }
+        }
+        shape = rotatedShape;
+    }
 
-	// Rotación de la pieza en el sentido contrario a las agujas del reloj
-	public void rotateCounterClockwise() {
-	    int rows = shape.length;
-	    int cols = shape[0].length;
-	    boolean[][] rotatedShape = new boolean[cols][rows];
-	
-	    for (int i = 0; i < rows; i++) {
-	        for (int j = 0; j < cols; j++) {
-	            rotatedShape[cols - 1 - j][i] = shape[i][j];
-	        }
-	    }
-	    shape = rotatedShape;
-	}
+    // Rotación de la pieza en el sentido contrario a las agujas del reloj
+    public void rotateCounterClockwise() {
+        int rows = shape.length;
+        int cols = shape[0].length;
+        boolean[][] rotatedShape = new boolean[cols][rows];
 
-	public static Piece generateRandomPiece() {
-	    Random random = new Random();
-	    boolean[][][] shapes = {
-	        // Pieza cuadrada
-	        {{true, true}, {true, true}},
-	        // Pieza en forma de línea
-	        {{true, true, true, true}},
-	        // Pieza en forma de T
-	        {{false, true, false}, {true, true, true}},
-	        // Pieza en forma de L
-	        {{true, false}, {true, false}, {true, true}},
-	        // Pieza en forma de Z
-	        {{true, true, false}, {false, true, true}},
-	        // Pieza en forma de J
-	        {{false, true}, {false, true}, {true, true}},
-	        // Pieza en forma de S
-	        {{false, true, true}, {true, true, false}}
-	    };
-	
-	    boolean[][] randomShape = shapes[random.nextInt(shapes.length)];
-	    return new Piece(randomShape);
-	}
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                rotatedShape[cols - 1 - j][i] = shape[i][j];
+            }
+        }
+        shape = rotatedShape;
+    }
 
-	// Devuelve el ancho de la pieza (para validaciones de colisión y límites)
-	public int getWidth() {
-	    return shape[0].length;
-	}
-	
-	// Devuelve la altura de la pieza
-	public int getHeight() {
-	    return shape.length;
-	}
-	
-	// Coloca la pieza en una posición específica del tablero
-	public void setPosition( final int newX, final int newY) {
-	    this.x = newX;
-	    this.y = newY;
-	}
+    /**
+     * Genera una pieza aleatoria.
+     *
+     * @return Una nueva pieza aleatoria.
+     */
+    public static Piece generateRandomPiece() {
+        Random random = new Random();
+        boolean[][][] shapes = {
+            // Pieza cuadrada
+            {{true, true}, {true, true}},
+            // Pieza en forma de línea
+            {{true, true, true, true}},
+            // Pieza en forma de T
+            {{false, true, false}, {true, true, true}},
+            // Pieza en forma de L
+            {{true, false}, {true, false}, {true, true}},
+            // Pieza en forma de Z
+            {{true, true, false}, {false, true, true}},
+            // Pieza en forma de J
+            {{false, true}, {false, true}, {true, true}},
+            // Pieza en forma de S
+            {{false, true, true}, {true, true, false}}
+        };
+
+        boolean[][] randomShape = shapes[random.nextInt(shapes.length)];
+        return new Piece(randomShape);
+    }
+
+    // Devuelve el ancho de la pieza (para validaciones de colisión y límites)
+    public int getWidth() {
+        return shape[0].length;
+    }
+
+    // Devuelve la altura de la pieza
+    public int getHeight() {
+        return shape.length;
+    }
+
+    // Coloca la pieza en una posición específica del tablero
+    public void setPosition(final int newX, final int newY) {
+        this.x = newX;
+        this.y = newY;
+    }
 }
